6c1ec049feb229d0cceaf4a86357319d19a1a55c
Arvind2698 <r.arvind007@gmail.com>
2020-05-23 23:29:37 +0530

    UC2 Multiple flip with head/tail win counter

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,12 +2,26 @@
 
 echo Welcome to the coin flip program
 
-flip=$((RANDOM%2))
+read -p "How many times do you want to flip the count" iteration
 
-if [[ $flip -eq 1 ]]
-then
-	echo HEADS
-else
-	echo TAILS
-fi
+#variables
+headWinCounter=0
+tailWinCounter=0
+
+
+for ((count=0;count<$iteration;count++))
+do
+	flip=$((RANDOM%2))
+
+	if [[ $flip -eq 1 ]]
+	then
+		((headWinCounter++))
+	else
+		((tailWinCounter++))
+	fi
+done
+
+echo Number of times HEAD won: $headWinCounter
+echo Number of times TAIL won: $tailWinCounter
+
 
