868ea2b41bbbee4e7c25e887718ec7bea22e4b2f
Arvind2698 <r.arvind007@gmail.com>
2020-05-24 00:10:51 +0530

    UC4 update to UC3 with added functionality to the tie case

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -21,7 +21,28 @@ done
 
 if [[ $headWinCounter -eq $tailWinCounter ]]
 then
-	echo We have a tie
+	echo We have a tie. Rolling again........
+
+	while [[ $(($headWinCounter-$tailWinCounter)) -lt 2 && $(($tailWinCounter-$headWinCounter)) -lt 2  ]]
+	do
+   	flip=$((RANDOM%2))
+
+   	if [[ $flip -eq 1 ]]
+   	then
+      	((headWinCounter++))
+   	else
+      	((tailWinCounter++))
+   	fi
+	done
+
+	if [[ $headWinCounter -gt $tailWinCounter ]]
+	then
+   	echo HEADS has won by $((headWinCounter-tailWinCounter)) tosses
+	else
+   	echo TAILS has won by $((tailWinCounter-headWinCounter)) tosses
+	fi
+
+
 elif [[ $headWinCounter -gt $tailWinCounter ]]
 then
 	echo HEADS has won by $((headWinCounter-tailWinCounter)) tosses
